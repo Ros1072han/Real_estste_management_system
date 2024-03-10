38872a9d99dd42cf7da5dad3ec0bbcdd9df4faf4
Roshan kumar <roshankmr10072002@gmail.com>
2024-03-10 09:04:23 +0530

    main

---
+++ pages/views.py
@@ -0,0 +1,51 @@
+import base64
+from io import BytesIO
+
+import qrcode
+from django.shortcuts import render
+from listings.choices import price_choices, bedroom_choices, state_choices
+from listings.models import Listing
+from realtors.models import Realtor
+
+
+def index(request):
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+
+    context = {
+        'listings': listings,
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices
+    }
+
+    return render(request, 'pages/index.html', context)
+
+
+def about(request):
+    # Get all realtors
+    realtors = Realtor.objects.order_by('-hire_date')
+
+    # Get MVP
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+
+    context = {
+        'realtors': realtors,
+        'mvp_realtors': mvp_realtors
+    }
+
+    return render(request, 'pages/about.html', context)
+
+
+def qwerty(request):
+    context = {}
+    if request.method == "POST":
+        qr_text = request.POST.get("qr_text", "")
+        qr_image = qrcode.make(qr_text, box_size=15)
+        qr_image_pil = qr_image.get_image()
+        stream = BytesIO()
+        qr_image_pil.save(stream, format='PNG')
+        qr_image_data = stream.getvalue()
+        qr_image_base64 = base64.b64encode(qr_image_data).decode('utf-8')
+        context['qr_image_base64'] = qr_image_base64
+        context['variable'] = qr_text
+    return render(request, 'listings/insta.html', context=context)
